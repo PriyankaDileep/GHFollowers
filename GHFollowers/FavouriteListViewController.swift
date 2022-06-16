3ce2999c1493dc5ad974829f2cfcbe1fcea27c0a
Priyanka <priyauthram@yahoo.co.in>
2022-06-16 13:50:57 +1000

    Added app icon images and project related images for dark and night mode

---
+++ GHFollowers/FavouriteListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteListViewController.swift
+//  GHFollowers
+//
+//  Created by Priyanka PS on 16/6/22.
+//
+
+import UIKit
+
+class FavouriteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+}
--- GHFollowers/SearchViewController.swift
+++ GHFollowers/SearchViewController.swift
@@ -1,19 +1,19 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GHFollowers
 //
-//  Created by Priyanka PS on 6/6/22.
+//  Created by Priyanka PS on 16/6/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
